0066fc5957cf896f7f2ab5fdf41e44cd4af90eb1
Pamela Badermann <badermann.pamela@gmail.com>
2022-05-09 00:10:44 -0300

    express

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/users', (req, res) => {
+  return res.send('Hello World');
+})
+
 app.listen(3333, () => {
   console.log('Server started on port 3333');
 });
