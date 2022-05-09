04ad286052ef92d07c779b03a355fda536798dc6
Pamela Badermann <badermann.pamela@gmail.com>
2022-05-09 01:51:46 -0300

    setting nodemailer

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "bdb3fd582ac96e",
+    pass: "30c346f892e523"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Pamela Badermann <badermann.pamela@gmail.cpm>',
+    subject: 'Novo feedback',
+    html: [
+      `div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n'),
   })
 
   return res.status(201).json({ data: feedback });
