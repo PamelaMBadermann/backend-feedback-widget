648314cc7efb3327099f7b613af329b990387d97
Pamela Badermann <badermann.pamela@gmail.com>
2022-05-09 01:28:47 -0300

    setting prisma

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send('Hello World');
-})
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  return res.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
